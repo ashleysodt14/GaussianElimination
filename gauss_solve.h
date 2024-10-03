57a7b16266345daad3bbf00ee0fd12d1e5ec1fc4
ashleysodt14 <acsodt@arizona.edu>
2024-10-03 10:58:49 -0700

    Update gauss_solve.h

--- gauss_solve.h
+++ gauss_solve.h
@@ -1,3 +1,13 @@
+/*----------------------------------------------------------------
+* File:     gauss_solve.h
+*----------------------------------------------------------------
+*
+* Author:   Marek Rychlik (rychlik@arizona.edu)
+* Date:     Sun Sep 22 15:40:51 2024
+* Copying:  (C) Marek Rychlik, 2020. All rights reserved.
+*
+*----------------------------------------------------------------*/
+
 #ifndef GAUSS_SOLVE_H
 #define GAUSS_SOLVE_H
 
