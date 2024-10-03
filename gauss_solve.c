0a45f7f93c9ba34805589f59ba5d670e64383deb
ashleysodt14 <acsodt@arizona.edu>
2024-10-02 18:24:33 -0700

    Update gauss_solve.c

--- gauss_solve.c
+++ gauss_solve.c
@@ -1,7 +1,8 @@
 #include "gauss_solve.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h> // Include math.h for fabs function
+#include <math.h>  // Include math.h for fabs function
+#include <Python.h>  // Include Python API
 
 void plu(int n, double A[n][n], int P[n]) {
     double L[n][n];  // Temporary lower triangular matrix
@@ -53,68 +54,4 @@ void plu(int n, double A[n][n], int P[n]) {
     // Copy U and L to the appropriate positions in A
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i < j) {
-                A[i][j] = U[i][j];  // Upper triangular part
-            } else if (i == j) {
-                A[i][j] = 1.0;  // Diagonal of L is 1
-            } else {
-                A[i][j] = L[i][j];  // Lower triangular part
-            }
-        }
-    }
-}
-
-void gauss_solve_in_place(const int n, double A[n][n], double b[n]) {
-    for (int k = 0; k < n; ++k) {
-        for (int i = k + 1; i < n; ++i) {
-            /* Store the multiplier into A[i][k] as it would become 0 and be
-            useless */
-            A[i][k] /= A[k][k];
-            for (int j = k + 1; j < n; ++j) {
-                A[i][j] -= A[i][k] * A[k][j];
-            }
-            b[i] -= A[i][k] * b[k];
-        }
-    } /* End of Gaussian elimination, start back-substitution. */
-    for (int i = n - 1; i >= 0; --i) {
-        for (int j = i + 1; j < n; ++j) {
-            b[i] -= A[i][j] * b[j];
-        }
-        b[i] /= A[i][i];
-    } /* End of back-substitution. */
-}
-
-void lu_in_place(const int n, double A[n][n]) {
-    for (int k = 0; k < n; ++k) {
-        for (int i = k; i < n; ++i) {
-            for (int j = 0; j < k; ++j) {
-                /* U[k][i] -= L[k][j] * U[j][i] */
-                A[k][i] -= A[k][j] * A[j][i];
-            }
-        }
-        for (int i = k + 1; i < n; ++i) {
-            for (int j = 0; j < k; ++j) {
-                /* L[i][k] -= A[i][k] * U[j][k] */
-                A[i][k] -= A[i][j] * A[j][k];
-            }
-            /* L[k][k] /= U[k][k] */
-            A[i][k] /= A[k][k];
-        }
-    }
-}
-
-void lu_in_place_reconstruct(int n, double A[n][n]) {
-    for (int k = n - 1; k >= 0; --k) {
-        for (int i = k + 1; i < n; ++i) {
-            A[i][k] *= A[k][k];
-            for (int j = 0; j < k; ++j) {
-                A[i][k] += A[i][j] * A[j][k];
-            }
-        }
-        for (int i = k; i < n; ++i) {
-            for (int j = 0; j < k; ++j) {
-                A[k][i] += A[k][j] * A[j][i];
-            }
-        }
-    }
-}
+            if
